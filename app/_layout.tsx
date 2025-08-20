048108df7d36bdf7d827c0b86b7161c6f100671d
lckhndleup <maliyildiz0307@gmail.com>
2025-08-21 01:18:12 +0300

    fix/bottom nav new - width and spacing fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,9 +19,6 @@ export default function RootLayout() {
             headerTitleStyle: {
               fontWeight: "bold",
             },
-            contentStyle: {
-              paddingBottom: 90, // BottomNavigation için alan bırakın
-            },
           }}
         >
           <Stack.Screen
