dee2104286650e00e8e9eea5c4ed79de88984491
lckhndleup <maliyildiz0307@gmail.com>
2025-09-08 21:15:57 +0300

    fix/ animasyon loop tamamlandı

--- app/index.tsx
+++ app/index.tsx
@@ -289,7 +289,6 @@ export default function HomePage() {
               />
             </View>
           </Card>
-          <Loading size="large" />
         </View>
       </ScrollView>
     </Container>
